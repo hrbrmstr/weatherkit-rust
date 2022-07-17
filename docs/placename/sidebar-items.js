b49f91fb416f79239ad8b9d6f6ba52cdcf6cf56a
hrbrmstr <bob@rud.is>
2022-07-17 12:10:18 -0400

    docs

---
+++ docs/placename/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"constant":[["WEATHERKIT_APP_INFO",""]],"fn":[["get_placename","Retrieve a place name from lat/lon pair via latlon.top"],["lookup_placename",""]],"struct":[["Place",""],["PlaceName",""]]};
